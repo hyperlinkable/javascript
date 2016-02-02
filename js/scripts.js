99072f002e298592406fc4cbc8f8511e12abf8c8
David Wynn and Priya Vasudevan <davidwwynn@gmail.com>
2016-02-01 20:49:11 -0800

    add jquery event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $("h1").click(function() {
     alert("This is a header.");
   });
+
+  $("h2").click(function() {
+    alert("This is one dummy paragraph.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").dblclick(function() {
+    alert("This is a picture.");
+  });
+
 });
